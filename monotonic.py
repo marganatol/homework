66504082721c8119fdca130bb5b67998d250d126
marganatol <92037641+marganatol@users.noreply.github.com>
2021-11-08 23:21:12 +0200

    Add files via upload

---
+++ monotonic.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+nums = list(map(int,input('Введите цифры через пробел :').split()))
+ 
+def is_monotonic(nums):
+    count = 1
+    for i in range (0, len(nums) - 1):
+        if (nums[i] <= nums[i + 1]):
+            count += 1
+    if (count == len(nums)):
+        return True
+    count = 1
+    for i in range (0, len(nums) - 1):
+        if (nums[i] >= nums[i + 1]):
+            count += 1
+    if (count == len(nums)):
+        return True
+    return False
+    
+if (is_monotonic(nums)):
+    print("True")
+else:
+    print("False")
